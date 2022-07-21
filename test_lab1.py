1bd99716ccf59d305a51d2fa58c43b94c1f2b331
EC2 Default User <ec2-user@ip-172-31-16-136.ap-southeast-1.compute.internal>
2022-07-21 02:17:44 +0000

    First lab of the day

---
+++ test_lab1.py
@@ -0,0 +1,11 @@
+def increment_by_one(i):
+    return i + 2
+    
+def decrement_by_one(j):
+    return j - 1
+    
+def test_answer_1():
+    assert increment_by_one(1) == 3
+
+def test_answer_2():
+    assert decrement_by_one(1) == 0
